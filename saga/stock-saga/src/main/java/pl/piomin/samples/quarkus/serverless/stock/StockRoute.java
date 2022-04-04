195a61e08e285e1ad30dbd8ce838455a3f13fd58
piomin <piotr.minkowski@gmail.com>
2022-04-04 16:28:24 +0200

    Update `stock-saga` with fixes

--- saga/stock-saga/src/main/java/pl/piomin/samples/quarkus/serverless/stock/StockRoute.java
+++ saga/stock-saga/src/main/java/pl/piomin/samples/quarkus/serverless/stock/StockRoute.java
@@ -1,34 +1,40 @@
 package pl.piomin.samples.quarkus.serverless.stock;
 
+import com.github.piomin.entity.model.customer.Customer;
+import com.github.piomin.entity.model.product.Product;
+import lombok.Data;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.model.rest.RestBindingMode;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
+
 // camel-k: trait=knative-service.enabled=true
+// camel-k: dependency=mvn:org.apache.camel.quarkus:camel-quarkus-core
 // camel-k: dependency=mvn:org.apache.camel.quarkus:camel-quarkus-jpa
 // camel-k: dependency=mvn:org.apache.camel.quarkus:camel-quarkus-jackson
 // camel-k: dependency=mvn:io.quarkus:quarkus-jdbc-postgresql
 // camel-k: dependency=mvn:org.projectlombok:lombok:1.18.22
 // camel-k: dependency=github:piomin/entity-model/1.1
 
-import com.github.piomin.entity.model.customer.Customer;
-import com.github.piomin.entity.model.product.Product;
-import lombok.Data;
-import org.apache.camel.Exchange;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.dataformat.JsonLibrary;
-
-import java.util.List;
-import java.util.Random;
-
+@ApplicationScoped
 public class StockRoute extends RouteBuilder {
 
-    static long i = 0;
+    private static long i = 0;
+    private final JacksonDataFormat FORMAT = new JacksonDataFormat(Order.class);
 
     @Override
     public void configure() throws Exception {
+        restConfiguration().bindingMode(RestBindingMode.json);
 
         rest("/stock")
-                .post("/reserve").consumes("application/json")
+                .post("/reserve").consumes("application/json").type(Order.class)
                 .route()
-                .log("Order received: ${body}")
-                .unmarshal().json(JsonLibrary.Jackson, Order.class)
+                    .log("Order received: ${body}")
                     .choice()
                         .when().simple("${body.status.toString()} == 'NEW'")
                             .setBody(exchange -> {
@@ -36,33 +42,35 @@ public class StockRoute extends RouteBuilder {
                                 order.setStatus(OrderStatus.IN_PROGRESS);
                                 return order;
                             })
-                            .marshal().json(JsonLibrary.Jackson)
+                            .marshal(FORMAT)
                             .log("Reservation sent: ${body}")
                             .toD("kafka:reserve-events?brokers=${env.KAFKA_BOOTSTRAP_SERVERS}")
                     .end()
-                .unmarshal().json(JsonLibrary.Jackson, Order.class)
-                .setProperty("orderItems", simple("${body.productCount}", Integer.class))
-                .setProperty("orderStatus", simple("${body.status}", OrderStatus.class))
-                .toD("jpa:" + Product.class.getName() + "?query=select p from Product p where p.id= ${body.productId}")
-                .choice()
-                    .when().simple("${exchangeProperty.orderStatus} == 'IN_PROGRESS'")
-                        .setBody(exchange -> {
-                            Product product = (Product) exchange.getIn().getBody(List.class).get(0);
-                            product.setItemsReserved(product.getItemsReserved() + exchange.getProperty("orderItems", Integer.class));
-                            product.setItemsAvailable(product.getItemsAvailable() - exchange.getProperty("orderItems", Integer.class));
-                            return product;
-                        })
-                    .otherwise()
-                        .setBody(exchange -> {
-                            Product product = (Product) exchange.getIn().getBody(List.class).get(0);
-                            product.setItemsReserved(product.getItemsReserved() - exchange.getProperty("orderItems", Integer.class));
-                            return product;
-                        })
+                    .unmarshal(FORMAT)
+                    .setProperty("orderItems", simple("${body.productCount}", Integer.class))
+                    .setProperty("orderStatus", simple("${body.status}", OrderStatus.class))
+                    .toD("jpa:" + Product.class.getName() + "?query=select p from Product p where p.id = ${body.productId}")
+                    .choice()
+                        .when().simple("${exchangeProperty.orderStatus} == 'IN_PROGRESS'")
+                            .setBody(exchange -> {
+                                Product product = (Product) exchange.getIn().getBody(List.class).get(0);
+                                product.setItemsReserved(product.getItemsReserved() + exchange.getProperty("orderItems", Integer.class));
+                                product.setItemsAvailable(product.getItemsAvailable() - exchange.getProperty("orderItems", Integer.class));
+                                return product;
+                            }).endChoice()
+                        .otherwise()
+                            .setBody(exchange -> {
+                                Product product = (Product) exchange.getIn().getBody(List.class).get(0);
+                                product.setItemsReserved(product.getItemsReserved() - exchange.getProperty("orderItems", Integer.class));
+                                return product;
+                            })
                     .end()
-                .log("Current product: ${body}")
-                .to("jpa:" + Customer.class.getName() + "?useExecuteUpdate=true")
-                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(201)).setBody(constant(null))
-                .endRest();
+                    .log("Current product: ${body}")
+                    .to("jpa:" + Product.class.getName() + "?useExecuteUpdate=true")
+                    .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(201))
+                    .setBody(constant(null))
+                .end()
+        .endRest();
 
         Random r = new Random();
         from("timer://runOnce?repeatCount=1&delay=100")
@@ -75,7 +83,7 @@ public class StockRoute extends RouteBuilder {
 }
 
 @Data
-class Order {
+class Order implements Serializable {
     private Long id;
     private Long customerId;
     private int amount;
